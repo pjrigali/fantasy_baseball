da661751088bbf76e3f09bae861c5d7ce1bd4666
pjrigali <peterjrigali@gmail.com>
2025-03-17 16:15:53 -0500

    csv handling funcs

--- functions.py
+++ functions.py
@@ -1,3 +1,5 @@
+import os
+import csv
 from datetime import datetime, timedelta
 
 
@@ -18,3 +20,44 @@ def get_weeks(start_dt, end_dt) -> list:
         weeks.append((first_monday, first_monday + timedelta(days=6)))
         first_monday += timedelta(days=7)
     return weeks
+
+
+def read_csv(file_name: str) -> list:
+    temp = []
+    with open(file_name, mode='r', encoding='utf-8') as f:
+        reader = csv.DictReader(f, delimiter=',')
+        for i in reader:
+            d = {}
+            for k, v in i.items():
+                if k.startswith('\ufeff'):
+                    k = k.replace('\ufeff', '')
+                if k.startswith('"') and k.endswith('"'):
+                    k = k.replace('"', '')
+                d[k] = v
+            temp.append(d)
+    return temp
+
+
+def write_csv(file_path: str, data: list, sort_cols: bool = True) -> None:
+    file_exists = os.path.exists(file_path)
+
+    with open(file_path, mode='a', encoding='utf-8', newline='') as file:
+        cols = set()
+        for r in data:
+            for k, v in r.items():
+                if k not in cols:
+                    cols.add(k)
+        cols = list(cols)
+
+        if sort_cols: 
+            cols.sort()
+
+        writer = csv.DictWriter(file, fieldnames=cols)
+        
+        if not file_exists:
+            writer.writeheader()
+
+        writer.writerows(data)
+    return None
+
+
